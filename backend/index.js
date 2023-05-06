41acac9f260997b606488ebc5bd60b511b341e20
Luch <lperotti39@gmail.com>
2023-05-06 07:49:11 -0400

    Set up backend, client folder structure.  Build index.js file.

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+//mongodb conect
+
+//start server
